dacf7493e795487d78e44d66c9558d4dca2bdfd4
june <djunee0@gmail.com>
2026-05-21 15:38:27 +0700

    feat: add Tailwind CSS and PostCSS configuration
    - Updated package.json to include Tailwind CSS, PostCSS, and related dependencies.
    - Created postcss.config.js for PostCSS setup with Tailwind CSS and Autoprefixer.
    - Added resources/css/pecut.css for Tailwind CSS styles and custom utilities.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Modified vite.config.js to include React plugin and updated input paths for assets.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/pecut/main.jsx",
             ],
             refresh: true,
         }),
+        react(),
     ],
-    server: {
-        host: "127.0.0.1",
-        port: 9000,
-    },
 });
